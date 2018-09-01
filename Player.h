8e9305e31a801d70c8730bb5406463a4d12762cf
MetalCat <36876492+CreativeMetalCat@users.noreply.github.com>
2018-09-01 14:54:45 +1000

    Player system reworked
    MoveDirection enum now in the Actor.h

--- Player.h
+++ Player.h
@@ -1,25 +1,24 @@
 #pragma once
 #include "Drawable.h"
-enum MoveDirection
-{
-	//If object is not suposed to be moved, or haven't been yet
-	NONE,
-	UP=1,
-	DOWN,
-	LEFT,
-	RIGHT,
-};
-class Player :public Drawable
+#include "Animation.h"
+class Player
 {
 public:
 	int WeapomId = 0;
 	int Rotation = 0;
-	int Layer = 0;
 	bool DrawImageAsAnim = false;
+	AnimatedSprite*image;
 	Gdiplus::RectF collision = Gdiplus::RectF(0, 0, 0, 0);
 	MoveDirection LastDir = MoveDirection::NONE;
-	Player(Gdiplus::Image*image, Gdiplus::RectF image_rect, Gdiplus::RectF collision);
-	Player(Gdiplus::Image*image, Gdiplus::PointF pt, Gdiplus::RectF collision);
+	Player(AnimatedSprite*image, Gdiplus::RectF image_rect, Gdiplus::RectF collision);
+	Player( AnimatedSprite*image, Gdiplus::PointF pt, Gdiplus::RectF collision);
+	Player(int layer, int AnimationFrameDelay,bool AnimationIsLooped,Gdiplus::Image*image, Gdiplus::RectF image_rect, Gdiplus::RectF collision);
+	Player(int layer, int AnimationFrameDelay, bool AnimationIsLooped, Gdiplus::Image*image, Gdiplus::PointF pt, Gdiplus::RectF collision);
+	float GetX() { return this->image->Image_Rect.X; }
+	float GetY() { return this->image->Image_Rect.Y; }
+	void SetX(float x) { this->image->Image_Rect.X = x; }
+	void SetY(float y) { this->image->Image_Rect.Y = y; }
+	int GetLayer()const { return this->image->Layer; }
+	void SetLayer(int layer) { this->image->Layer = layer; }
 	~Player();
 };
-
