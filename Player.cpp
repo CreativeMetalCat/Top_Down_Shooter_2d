f8f4ea6875876d1aadd67af532f92047e67b97e7
MetalCat <36876492+CreativeMetalCat@users.noreply.github.com>
2018-09-01 14:55:23 +1000

    Added better animation support

--- Player.cpp
+++ Player.cpp
@@ -2,14 +2,27 @@
 #include "Player.h"
 
 
-Player::Player(Gdiplus::Image*image, Gdiplus::RectF image_rect, Gdiplus::RectF collision):Drawable(image,image_rect)
+Player::Player(AnimatedSprite*image, Gdiplus::RectF image_rect, Gdiplus::RectF collision)
 {
 	this->collision = collision;
+	this->image = image;
 }
 
-Player::Player(Gdiplus::Image*image, Gdiplus::PointF pt, Gdiplus::RectF collision) : Drawable(image, pt)
+Player::Player(AnimatedSprite*image, Gdiplus::PointF pt, Gdiplus::RectF collision)
+{
+	this->image = image;
+	this->collision = collision;
+}
+Player::Player(int layer, int AnimationFrameDelay, bool AnimationIsLooped, Gdiplus::Image*image, Gdiplus::RectF image_rect, Gdiplus::RectF collision)
 {
 	this->collision = collision;
+	this->image = new AnimatedSprite(AnimationFrameDelay, AnimationIsLooped, image, image_rect, true, layer);
+}
+
+Player::Player(int layer, int AnimationFrameDelay, bool AnimationIsLooped, Gdiplus::Image*image, Gdiplus::PointF pt, Gdiplus::RectF collision)
+{
+	this->collision = collision;
+	this->image = new AnimatedSprite(AnimationFrameDelay, AnimationIsLooped, image, Gdiplus::RectF(pt.X,pt.Y,image->GetWidth(),image->GetHeight()), true, layer);
 }
 
 Player::~Player()
